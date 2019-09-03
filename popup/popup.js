12a1aadad2a68051273f4c561351e23ba3f018ae
maxkurylo <kurylo.max@gmail.com>
2019-09-03 17:46:59 +0300

    manifest file formatting

--- popup/popup.js
+++
@@ -1,7 +0,0 @@
-const hostnameInput = document.getElementById('hostname')
-
-const query = { active: true, currentWindow: true }
-
-chrome.tabs.query(query, tabs => {
-    hostnameInput.value = tabs[0].url
-})
